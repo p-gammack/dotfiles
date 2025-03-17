d3c0fc7bf7c14d455972f9ed2fcd141bcc5add56
Patrick Gammack <56632002+p-gammack@users.noreply.github.com>
2025-03-17 17:49:24 +0000

    nvim: recenter when doing half jumps up & down

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,3 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+
+-- CUSTOM MAPPINGS
+
+-- Move up half a screen and recenter
+vim.keymap.set("n", "<C-U>", "<C-U>zz", { noremap = true, silent = true })
+
+-- Move down half a screen and recenter
+vim.keymap.set("n", "<C-D>", "<C-D>zz", { noremap = true, silent = true })
